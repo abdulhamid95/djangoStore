70afa4c244c1afa5a4e89046300410b3e9faaf14
abdulhamid95 <abdulhamidhamsho1@gmail.com>
2026-02-05 10:30:28 +0300

    add database info

--- django_ecommerce/settings.py
+++ django_ecommerce/settings.py
@@ -87,11 +87,11 @@ WSGI_APPLICATION = 'django_ecommerce.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'store-online',
-        'USER': 'postgres',
-        'PASSWORD': '12345',
-        'HOST': 'localhost',
-        'PORT': '5432',
+        'NAME': config('DB_NAME'),
+        'USER': config('DB_USER'),
+        'PASSWORD': config('DB_PASSWORD'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
     }
 }
 
